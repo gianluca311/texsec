bec193de4197886413333604abb5e6c782000f16
Gianluca <gianluca.raberger@me.com>
2017-05-06 10:21:01 +0200

    upload refactored
    RPC call implemented

--- api/latex.go
+++ api/latex.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net/rpc"
 	"os"
 	"strconv"
 	"sync"
@@ -12,25 +14,19 @@ import (
 	"github.com/gianluca311/texsec/api/app"
 	"github.com/goadesign/goa"
 	"github.com/satori/go.uuid"
+	"gopkg.in/h2non/filetype.v1"
 )
 
 // LatexController implements the latex resource.
 type LatexController struct {
 	*goa.Controller
 	*sync.Mutex
-	archive *LatexUploadArchiveData
-}
-
-type LatexUploadArchiveData struct {
-	UUID       string
-	FileName   string
-	UploadedAt time.Time
 }
 
 type ResponseMessage struct {
-	OK      bool
-	UUID    string
-	Message string
+	OK      bool   `json:"ok"`
+	UUID    string `json:"uuid"`
+	Message string `json:"message"`
 }
 
 // NewLatexController creates a latex controller.
@@ -65,18 +61,39 @@ func (c *LatexController) Upload(ctx *app.UploadLatexContext) error {
 	// LatexController_Upload: start_implement
 
 	// Put your logic here
+	uuid := uuid.NewV4().String()
+	uploadedAt := time.Now()
 
 	debug, _ := strconv.ParseBool(ctx.PostFormValue("debug"))
 	maxDownloads, _ := strconv.Atoi(ctx.PostFormValue("max_downloads"))
+
 	//FIX ME
 	file, handler, err := ctx.FormFile("file")
 	if err != nil {
 		return goa.ErrBadRequest("failed to load file: %s", err.Error())
 	}
 	defer file.Close()
+
+	head := make([]byte, 261)
+	_, err = file.Read(head)
+	if err != nil {
+		return goa.ErrBadRequest("unable to read file: %s", err.Error())
+	}
+
+	if !filetype.IsArchive(head) {
+		res := &ResponseMessage{
+			OK:      false,
+			Message: "Uploaded file isn't an archive",
+		}
+		resJSON, _ := json.Marshal(res)
+		return ctx.NotAcceptable(resJSON)
+	}
+
+	fileKind, _ := filetype.Match(head)
+
 	var archive *app.LatexArchive
 
-	fileName := "latex-archive-" + time.Now().String() + handler.Filename
+	fileName := "latex-archive-" + uuid + fileKind.Extension
 	f, err := os.OpenFile("./archives/"+fileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return fmt.Errorf("failed to save file: %s", err)
@@ -85,29 +102,24 @@ func (c *LatexController) Upload(ctx *app.UploadLatexContext) error {
 	defer f.Close()
 	io.Copy(f, file)
 
-	data := c.saveArchive(fileName)
-	archive = &app.LatexArchive{UUID: data.UUID, Filename: data.FileName, UploadedAt: data.UploadedAt}
+	archive = &app.LatexArchive{UUID: uuid, Filename: fileName, UploadedAt: uploadedAt}
 
 	res := &ResponseMessage{
 		OK:      true,
 		UUID:    archive.UUID,
-		Message: "Upload success full. debug set to: " + strconv.FormatBool(debug) + " max_downloads: " + strconv.Itoa(maxDownloads) + " Proccess UUID: " + archive.UUID,
+		Message: "Upload of " + handler.Filename + " successfull. debug set to: " + strconv.FormatBool(debug) + ". max_downloads: " + strconv.Itoa(maxDownloads) + " Proccess UUID: " + archive.UUID,
 	}
+
+	client, err := rpc.DialHTTP("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	var resp ResponseMessage
+	client.Call("StartProcessing", archive, &resp)
 
 	resJSON, _ := json.Marshal(res)
 	ctx.OK(resJSON)
 	// LatexController_Upload: end_implement
 	return nil
-}
-
-func (c *LatexController) saveArchive(filename string) *LatexUploadArchiveData {
-	c.Lock()
-	defer c.Unlock()
-	data := &LatexUploadArchiveData{
-		UUID:       uuid.NewV4().String(),
-		FileName:   filename,
-		UploadedAt: time.Now(),
-	}
-	c.archive = data
-	return data
 }
