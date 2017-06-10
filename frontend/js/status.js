f5d5faf91c32dbe5fda3784d9cb68b64880ca7f5
Gianluca <gianluca.raberger@me.com>
2017-06-10 15:50:46 +0200

    adapted status page according to api properties.

--- frontend/js/status.js
+++ frontend/js/status.js
@@ -20,7 +20,11 @@ document.querySelector("button[name=get_status]").onclick = function () {
         url: config.apiHost+"status/"+ uuid
     }).done(function(data) {
         document.querySelector("div#status-box").className = "panel panel-default";
-        body.innerText = data.message;
+        body.innerHTML = '<strong>UUID:</strong> ' + data.uuid + `<br />
+        <strong>Upload Time:</strong> ` + data.upload_time + `<br />
+        <strong>Max Downloads:</strong> ` + data.max_downloads + `<br />
+        <strong>Download Count:</strong> ` + data.download_count + `<br />
+        <strong>LOG:</strong><p>` + data.message;  
         body.innerHTML += '<a class="btn btn-warning" href="download.html?id='+uuid+'">Download</a>';
 
     }).fail(function(err) {
